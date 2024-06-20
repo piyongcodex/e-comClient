255836cd7e6111a5b1c3daa57b3e5eb50bde8bfe
vonviz <voncarlo0211@gmail.com>
2024-06-20 22:29:14 +0800

    added fetching product

--- src/users/products/ProductPage.jsx
+++ src/users/products/ProductPage.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { useState, useEffect } from "react";
 
 const ProductPage = () => {
-  const [product, setProduct] = useState([]);
+  const [showProduct, setProduct] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4005/b5/products/active")
+    fetch(
+      "http://ec2-3-145-114-4.us-east-2.compute.amazonaws.com/b5/products/active"
+    )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setProduct(data.products);
+      });
   }, []);
 
   return (
     <Container>
       <Row>
-        <Col className="p-5 text-center">Product</Col>
+        {showProduct.map((product) => (
+          <Col key={product.id} className="p-5 text-center">
+            <div>
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <p>Price: ${product.price}</p>
+            </div>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
