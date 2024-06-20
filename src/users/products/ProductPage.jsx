385df15890210c0ee2ad3fd53a2b222002b46e21
rio.sumandal0907 <rio.sumandal0907@gmail.com>
2024-06-20 22:19:16 +0800

    update

--- src/users/products/ProductPage.jsx
+++ src/users/products/ProductPage.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 const ProductPage = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4005/b5/products/active")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <Container>
       <Row>
-        <Col className="p-5 text-center">
-          <ul>
-            <li>ulam</li>
-            <li>sarap</li>
-            <li>Diwata Pares</li>
-          </ul>
-        </Col>
+        <Col className="p-5 text-center">Product</Col>
       </Row>
     </Container>
   );
